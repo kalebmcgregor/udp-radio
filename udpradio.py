2cff49c40b6f218cab3eb06d4c0d70848288f344
Kaleb McGregor <kaleb.mcgregor@gmail.com>
2018-03-06 11:01:04 -0800

    Fix Sender and Receiver classes and update main function

--- udpradio.py
+++ udpradio.py
@@ -3,98 +3,87 @@ import time
 import socket
 
 
-class sender:
+class Sender:
     def __init__(self, **kwargs):
         if not 'width' in kwargs:
-            width = 2
+            self.width = 2
         if not 'channels' in kwargs:
-            channels = 2
+            self.channels = 2
         if not 'rate' in kwargs:
-            rate = 44100
+            self.rate = 44100
         if not 'udp_ip' in kwargs:
-            udp_ip = "127.0.0.1"
+            self.udp_ip = "127.0.0.1"
         if not 'udp_port' in kwargs:
-            udp_port = 5005
-        
-        self.width = width
-        self.channels = channels
-        self.rate = rate
-        self.udp_ip = udp_ip
-        self.udp_port = udp_port
+            self.udp_port = 5005
+
         self.p = pyaudio.PyAudio()
-        stream = self.p.open(format=self.p.get_format_from_width(self.width), 
-                             channels=self.channels, 
-                             rate=self.rate, 
-                             input=False, 
-                             output=True, 
-                             stream_callback=callback)
+        self.stream = self.p.open(format=self.p.get_format_from_width(self.width),
+                                  channels=self.channels,
+                                  rate=self.rate,
+                                  input=False,
+                                  output=True,
+                                  stream_callback=self.callback)
         
-        sock = socket.socket(socket.AF_INET,
-                             socket.SOCK_DGRAM)
+        self.sock = socket.socket(socket.AF_INET,
+                                  socket.SOCK_DGRAM)
     
-    def callback(in_data, frame_count, time_info, flag):
-        data, addr = sock.recvfrom(10000)
+    def callback(self, in_data, frame_count, time_info, flag):
+        data, addr = self.sock.recvfrom(10000)
         return data, pyaudio.paContinue
     
-    def start():
-        sock.bind((UDP_IP, UDP_PORT))
-        stream.start_stream()
-        while stream.is_active():
+    def start(self):
+        self.sock.bind((self.udp_ip, self.udp_port))
+        self.stream.start_stream()
+        while self.stream.is_active():
             time.sleep(0.1)
 
-    def stop():
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
+    def stop(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
  
 
-class receiver:
+class Receiver:
     def __init__(self, **kwargs):
         if not 'width' in kwargs:
-            width = 2
+            self.width = 2
         if not 'channels' in kwargs:
-            channels = 2
+            self.channels = 2
         if not 'rate' in kwargs:
-            rate = 44100
+            self.rate = 44100
         if not 'udp_ip' in kwargs:
-            udp_ip = "127.0.0.1"
+            self.udp_ip = "127.0.0.1"
         if not 'udp_port' in kwargs:
-            udp_port = 5005
-        
-        self.width = width
-        self.channels = channels
-        self.rate = rate
-        self.udp_ip = udp_ip
-        self.udp_port = udp_port
+            self.udp_port = 5005
+
         self.p = pyaudio.PyAudio()
-        stream = p.open(format=self.p.get_format_from_width(self.width),
-                        channels=self.channels,
-                        rate=self.rate,
-                        input=True,
-                        output=False,
-                        stream_callback=callback)
+        self.stream = self.p.open(format=self.p.get_format_from_width(self.width),
+                                  channels=self.channels,
+                                  rate=self.rate,
+                                  input=True,
+                                  output=False,
+                                  stream_callback=self.callback)
         
-        sock = socket.socket(socket.AF_INET,
+        self.sock = socket.socket(socket.AF_INET,
                              socket.SOCK_DGRAM)
     
-    def callback(in_data, frame_count, time_info, flag):
-        sock.sendto(in_data, (UDP_IP, UDP_PORT))
+    def callback(self, in_data, frame_count, time_info, flag):
+        self.sock.sendto(in_data, (self.udp_ip, self.udp_port))
         return in_data, pyaudio.paContinue
 
-    def start():
-        stream.start_stream()
-        while stream.is_active():
+    def start(self):
+        self.stream.start_stream()
+        while self.stream.is_active():
+            print("In loop!")
             time.sleep(0.1)
 
-    def stop():
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
+    def stop(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
 
 if __name__ == "__main__":
-     walkie_sender = sender()
-     walkie_receiver = receiver()
-     sender.start
-     sender.stop
-     receiver.start
-     receiver.stop
+    walkie_sender = Sender()
+    walkie_receiver = Receiver()
+    walkie_sender.start()
+    walkie_receiver.start()
